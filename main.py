90934f2d4e72e8e0213e1d977c92a7b8cad1c9b0
Andrew Williams <110430206+andrew-programs@users.noreply.github.com>
2022-11-14 23:45:00 -0700

    refactoring and adding code to main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from matplotlib import pyplot
+import matplotlib.pyplot as pyplot
 import CSVConversion as conv
 import pandas
 import numpy
 import csv
 
-def main() -> None:
-    rows = conv.toRows("./input/bankdata.csv")
-    headerRow = [rows[0].pop(index) for index in range(len(rows[0]))]
-    dataFrame = pandas.DataFrame(rows, columns=headerRow)
-
-    print(dataFrame)
+def main(xInput: str, yInput: str) -> None:
+    cols = conv.toColumns("./input/bankdata.csv")
+    headerRow = [cols[index].pop(0) for index in range(len(cols))]
     
+    if xInput in headerRow and yInput in headerRow:
+        try:
+            dataFrame = pandas.DataFrame(rows, columns=headerRow)
+            dataFrame.plot()
+        except Exception as e:
+            print(f"Exception raised: {e}")
+    else:
+        raise ValueError("Either the x-value or y-value was not found within the header row, please double check capitalization/spelling.")
 
 if __name__ == "__main__":
-    main()
+    xInput = input("What would you like the x-value to be? > ")
+    yInput = input("What would you like the y-value to be? > ")
+
+    main(xInput, yInput)
